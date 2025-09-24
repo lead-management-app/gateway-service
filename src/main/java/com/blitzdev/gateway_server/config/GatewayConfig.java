3f17e2f0abcc5a446ee49c2cbdd4851c17c66ec0
theKelvinCode <thekelvincode@gmail.com>
2025-09-24 21:58:31 +0100

    Updates to config-server,naming-server,gateway-server.

---
+++ src/main/java/com/blitzdev/gateway_server/config/GatewayConfig.java
@@ -0,0 +1,16 @@
+package com.blitzdev.gateway_server.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .build();
+    }
+}
